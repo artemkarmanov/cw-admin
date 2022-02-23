e88ab667e9bf4e9100385c750f202e5e8b329d44
pavka <d.pavka@gmail.com>
2022-02-23 16:32:09 +0200

    Now pages are checked onLoad not onActivate

--- src/app/core/user.guard.ts
+++ src/app/core/user.guard.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {CanLoad, Router, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 import {environment} from '../../environments/environment';
 import {getStartPage} from './utils';
@@ -7,14 +7,13 @@ import {getStartPage} from './utils';
 @Injectable({
     providedIn: 'root'
 })
-export class UserGuard implements CanActivate {
+export class UserGuard implements CanLoad {
     constructor(private router: Router) {
     }
 
-    canActivate(
-        route: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    canLoad(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
         return environment.role !== 'admin' || this.router.createUrlTree([getStartPage()]);
     }
+
 
 }
