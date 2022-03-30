0a0f789e364772938c8dd07af30682901687caf2
maximLyakhov <maxim.lyakhov@gmail.com>
2022-03-30 13:37:19 +0300

    environment setup

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    backend: 'wss://testmain.captionworks.com:3000/socket',
-    role: 'viewer',
-    viewerUrl: 'https://testweb.captionworks.com:8082'
-};
