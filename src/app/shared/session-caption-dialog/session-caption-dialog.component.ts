f690c218d7843c82a17100f11affe8d968375be7
Ian Harris <imharris@gmail.com>
2022-03-31 09:45:59 +1100

    Merge remote-tracking branch 'origin/admin-caption-logs' into dev
    # Conflicts:
    #	src/app/main/bookings/bookings-page.component.html
    #	src/app/main/bookings/bookings-page.component.ts

--- src/app/shared/session-caption-dialog/session-caption-dialog.component.ts
+++ src/app/shared/session-caption-dialog/session-caption-dialog.component.ts
@@ -1,11 +1,9 @@
-import {AuthService} from 'src/app/core/auth.service';
 import {ISessionCaptionLogs} from 'src/app/core/types';
 import {ChangeDetectionStrategy, Component, OnDestroy, OnInit} from '@angular/core';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {BehaviorSubject, Observable, Subject, takeUntil} from 'rxjs';
-import {filter, map} from 'rxjs/operators';
+import {filter} from 'rxjs/operators';
 import {environment} from 'src/environments/environment';
-import {DateTime} from 'luxon';
 
 @Component({
     selector: 'cwb-session-caption-dialog',
@@ -17,14 +15,12 @@ export class SessionCaptionDialogComponent implements OnInit, OnDestroy {
     public isAdmin = environment.role === 'admin';
     public inEditMode = false;
     public data$$: BehaviorSubject<ISessionCaptionLogs[] | []> = new BehaviorSubject<ISessionCaptionLogs[] | []>([]);  
-    public userTimezone$: Observable<string> = this.authService.userSettings$
-        .pipe(map(data => data.timeZone));
     
     private destroy$$: Subject<void> = new Subject<void>();
     private data$: Observable<ISessionCaptionLogs[]> = this.data$$.asObservable().pipe(
         filter(Boolean)
     );
-    constructor(private modal: NgbActiveModal, private authService: AuthService) {
+    constructor(private modal: NgbActiveModal) {
     }
 
     ngOnInit(): void {
@@ -39,12 +35,5 @@ export class SessionCaptionDialogComponent implements OnInit, OnDestroy {
 
     close() {
         this.modal.dismiss();
-    }
-
-    getAdjustedHour(start: number, tz: string | null): string {
-        tz = tz === null ? 'America/New_York' : tz;
-        let dt = DateTime.fromMillis(start * 1000, {zone: tz});
-        let dater = dt.toISO({format: 'extended'}).toString();
-        return dater;
     }
 }
