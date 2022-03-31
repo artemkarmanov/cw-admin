3189a208ee847dc41b27ba1618d5079c724f8045
Artem Karmanov <artemkarmanov28@gmail.com>
2022-03-31 22:53:40 +0200

    removed comma, combined pipes

--- src/app/shared/adjust-time.pipe.ts
+++ src/app/shared/adjust-time.pipe.ts
@@ -1,14 +1,16 @@
-import {Pipe, PipeTransform} from '@angular/core';
+import {Inject, LOCALE_ID, Pipe, PipeTransform} from '@angular/core';
 import {DateTime} from 'luxon';
 import {map, Observable, of} from 'rxjs';
 import {AuthService} from 'src/app/core/auth.service';
-
+import {formatDate} from '@angular/common';
 @Pipe({
     name: 'adjustTime'
 })
 export class AdjustTimePipe implements PipeTransform {
 
-    constructor(private authService: AuthService) {}
+    constructor(
+        private authService: AuthService,
+        @Inject(LOCALE_ID) private locale: string) {}
 
     private userTimezone$: Observable<string> = this.authService.userSettings$
         .pipe(map(data => data.timeZone ? data.timeZone : 'America/New_York'));
@@ -23,7 +25,10 @@ export class AdjustTimePipe implements PipeTransform {
 
     private formatTime(start: number, tz: string) {
         if (!start) return String(start);
-        const dt = DateTime.fromMillis(start * 1000, {zone: tz});
-        return dt.toISO({format: 'extended'}).toString();
+        const dt = DateTime.fromMillis(start * 1000, {zone: tz})
+            .toISO({format: 'extended'}).toString()
+        const formattedDate = formatDate(dt, 'd MMM, y, h:mm a', this.locale)
+            .replace(',', '');
+        return formattedDate;
     }
 }
